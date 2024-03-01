4d01a4b890e5d935840edbf18a15d8b7ed1f4f68
Brent Nequin <brentnequin@gmail.com>
2024-03-01 00:35:06 -0600

    asdfadf

--- video_player/settings.py
+++ video_player/settings.py
@@ -15,6 +15,11 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static")
+]
+# STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Quick-start development settings - unsuitable for production
