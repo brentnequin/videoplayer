77b0366339bdab40e577cdf9746b6e16c69d6219
Brent Nequin <brentnequin@gmail.com>
2024-02-29 23:06:24 -0600

    debug false

--- video_player/settings.py
+++ video_player/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['APP_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 INTERNAL_IPS = ['127.0.0.1']
