84aec8aee2d92f25e00c66ce294f0f23c89b0d4a
Brent Nequin <brentnequin@gmail.com>
2024-03-01 00:18:00 -0600

    adfafdsdfs

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+from django.core.management import call_command
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'r', 'requirements.txt'])
 subprocess.check_call([sys.executable, 'manage.py', 'migrate'])
+subprocess.check_call([sys.executable, 'manage.py', 'collectstatic'])
