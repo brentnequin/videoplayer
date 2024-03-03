d17a232e280d07af33f439e2d8e3858f35df8f88
Brent Nequin <brentnequin@gmail.com>
2024-03-02 18:23:40 -0600

    oops

--- video_player/settings.py
+++ video_player/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = os.environ['APP_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 INTERNAL_IPS = ['127.0.0.1']
