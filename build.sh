354bb67b431ef8b4aa696cf1a2cee3adeeee8b11
Brent Nequin <brentnequin@gmail.com>
2024-03-02 20:21:03 -0600

    ye

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ pip install -r requirements.txt
 
 python3.9 manage.py migrate
 
-npm install --global rimraf
+python3.9 manage.py tailwind install
 python3.9 manage.py tailwind build
 
 python3.9 manage.py collectstatic  --noinput
