bc5e713a9b043e31db9900d4f40a42113bbb8013
Brent Nequin <brentnequin@gmail.com>
2024-03-02 18:52:36 -0600

    ewlkfm

--- build.py
+++ build.py
@@ -5,4 +5,5 @@ import sys
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'r', 'requirements.txt'])
 
 subprocess.check_call([sys.executable, 'manage.py', 'migrate'])
+subprocess.check_call([sys.executable, 'manage.py', 'tailwind', 'build'])
 subprocess.check_call([sys.executable, 'manage.py', 'collectstatic'])
