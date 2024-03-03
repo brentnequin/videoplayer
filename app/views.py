42eb7e7d1f7d18d5d52c87a02b3029b14895a4ee
Brent Nequin <brentnequin@gmail.com>
2024-03-03 15:42:13 -0600

    afdafdsfad

--- app/views.py
+++ app/views.py
@@ -2,23 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from app.models import Video
 from app.forms import UploadVideoForm
+from django.views.decorators.http import require_http_methods
 
 def index(request: HttpRequest):
-    data = {
-        'videos': Video.objects.order_by('-date')
-    }
-    return render(request, 'index.html', context=data)
 
-def new_video(request: HttpRequest):
-    return render(request, 'new-video.html')
+    return render(
+        request, 'index.html',
+        context={
+            'videos': Video.objects.order_by('-date')
+        }
+    )
 
-def upload(request: HttpRequest):  
-    form = UploadVideoForm(data=request.POST or None, files=request.FILES)
 
-    if not form.is_valid():
-        return HttpResponse(
-            f'<p class="error">Your form submission was unsuccessful ❌. Please would you correct the errors? The current errors: {form.errors}</p>')
+@require_http_methods(["GET", "POST"])
+def upload(request: HttpRequest):
 
-    video = Video(**form.cleaned_data, owner=request.user)
-    video.save()
-    return HttpResponse('Success')  
+    if request.method == 'GET':
+        return render(request, 'upload.html')
+    
+    elif request.method == 'POST':
+        form = UploadVideoForm(data=request.POST or None, files=request.FILES)
+
+        if not form.is_valid():
+            return HttpResponse(
+                f'<p class="error">Your form submission was unsuccessful ❌. Please would you correct the errors? The current errors: {form.errors}</p>')
+
+        video = Video(**form.cleaned_data, owner=request.user)
+        video.save()
+        return HttpResponse('Success')  
