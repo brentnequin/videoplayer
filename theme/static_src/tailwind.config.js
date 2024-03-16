96ae1dd6651040bcfbb3774aeb978f3c120799cb
Brent Nequin <brentnequin@gmail.com>
2024-03-16 13:04:26 -0500

    fix

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -40,6 +40,7 @@ module.exports = {
          * and make sure the pattern below matches your project structure.
          */
         // '../../**/*.py'
+        './node_modules/flowbite/**/*.js'
     ],
     theme: {
         extend: {},
@@ -58,5 +59,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('flowbite/plugin')
     ],
 }
